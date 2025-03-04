1c755dc29d7485dcc0fc626f233261823eded1a5
(Student  C00294397) Aleksandra Skarzycka <C00294397@setu.ie>
2025-03-04 12:51:06 +0000

    added global and changed page content

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log("Home page loaded");
+    });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+    
+</style>
