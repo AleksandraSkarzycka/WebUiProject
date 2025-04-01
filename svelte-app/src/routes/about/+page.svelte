f99418a2bd7171367d1ede351e392e09be46d4b0
(Student  C00294397) Aleksandra Skarzycka <C00294397@setu.ie>
2025-04-01 13:01:16 +0100

    about us page wip

--- svelte-app/src/routes/about/+page.svelte
+++ svelte-app/src/routes/about/+page.svelte
@@ -6,8 +6,156 @@
     });
 </script>
 
-<h1>About me</h1>
-<p>About me of the SvelteKit application</p>
+<h1>About Us</h1>
+
+<div id="info">
+<p>Chessington World of Adventures Resort now offers a theme park with magical rides and attractions a Zoo and SEALIFE centre with over 1000 animals, 
+and an onsite Resort Hotel with 150 bedrooms. However Chessington has not always offered this, explore our history and find out how the Chessington 
+you see today has changed over the years.</p>
+
+<h2> 1348 </h2> 
+<p> The original mansion was built in 1348, probably as a country house named Chessington Lodge. <br> </p>
+
+<h2> 1642 – 1660 </h2>
+<p> During the Civil War the original building became a Cavalier stronghold, and was burnt to the ground by Cromwell’s forces. Rebuilt after the restoration, it was renamed ‘Burnt Stub’, and has continued to be so known. <br> </p>
+
+<h2> 1660 – 1700 </h2>
+<p> The site was used as a wayside alehouse during the late 17th century. <br> </p>
+
+<h2> 1700 – 1900 </h2>
+<p> It is presumed that the farmstead was rebuilt on a larger scale in the 18th or 19th century. <br> </p>
+
+<h2> 1900 </h2>
+<p> The estate came into the hands of the Vere Barker family at the beginning of the 20th century, but was again seriously damaged by fire. It was reconditioned in 1919 at a cost of £50,000. <br> </p>
+
+<h2> 1931 </h2>
+<p> Animal enthusiast Reginald Goddard opened Chessington Zoo to allow the public to view his private animal collection. <br> </p>
+
+<h2> 1939 </h2>
+<p> In 1939, with World War Two impending, government restrictions prevented large crowds forming at entertainment venues during wartime, leading to the temporary closure of Chessington Zoo. <br> </p>
+
+<h2> 1978 </h2>
+<p> In 1978 Chessington was sold to the Pearson Group in 1978. Pearson later bought Madame Tussauds, and The Tussauds Group was formed. <br> </p>
+
+<h2> 1987 </h2>
+<p> Chessington World of Adventures opens as a theme park. Rides include Dragon Falls, Safari Skyway, Toadies Crazy Cars and Runaway Train. <br>
+
+The Magic Carpet opened <br> </p>
+
+<h2> 1988 </h2>
+<p> Black Buccaneer opened (Smugglers’ Galleon). <br> </p>
+
+<h2> 1989 </h2>
+<p> Dragon Falls’ Dragon was painted for the first time. (Dragon River) <br> </p>
+
+<h2> 1990 </h2>
+<p> The Vampire and Professor Burp’s Bubbleworks open in the new land Transylvania. <br> </p>
+
+<h2> 1991 </h2>
+<p> Following the development in 1990 there weren't any new attraction for 1991. <br> </p>
+
+<h2> 1992 </h2>
+<p> 1992 was another year of little investment. <br> </p>
+
+<h2> 1993 </h2>
+<p> 5th Dimension closed at the end of 1993. <br> </p>
+
+<h2> 1994 </h2>
+<p> Terror Tomb opened in the new land Forbidden Kingdom. <br> </p>
+
+<h2> 1995 </h2>
+<p> The Seastorm and Rameses Revenge rides are opened while Circus World closed. <br> </p>
+
+<h2> 1996 </h2>
+<p> Located in Calamity Canyon ‘Rodeo’ was the big news in 1996, The Carousel also opened near the South Entrance. <br> </p>
+
+<h2> 1997 </h2>
+<p> The Chessington Railroad closed and in the stations place a new Action Man adventure opened. <br> </p>
+
+<h2> 1998 </h2>
+<p> New for 1998 is The Rattlesnake, a mining-train rollercoaster rattling around an old Mexican Silver Mine. In the Zoo, the Creepy Caves open, with a wide range of slithering, scuttling and hair-raising creatures. Chessington also signs deals with KFC, McDonalds and Burger King to offer a range of food to visitors. <br> </p>
+
+<h2> 1999 </h2>
+<p> The Samurai ride opened as the ultimate endurance test with a mind-blowing 360˚ rotation from a height of over 60 feet. This attraction has moved to THORPE PARK. <br> </p>
+
+<h2> 2000 </h2>
+<p> Beanoland opens, bringing to life the mayhem and mischief of Dennis the Menace and his friends. Rides include Billy’s Wizzer (now Monkey Swinger) & Roger the Dodgers Dodgems (now Tuk Tuk Turmoil). <br> </p>
+
+<h2> 2001 </h2>
+<p> Berry Bouncers (now Treetop Hoppers) and Bash Street Bus (now Jungle Bus) are introduced. Mr Toadie’s Crazy Cars is also a popular addition. There are major developments to the Gorilla and big cat enclosures with the Trail of the Kings opening. <br> </p>
+
+<h2> 2002 </h2>
+<p> The Vampire ride is relaunched, and Tomb Blaster, an interactive laser ride mission, is introduced. In Beanoland, Dennis’ Mad House (now Temple of Mayhem) opened, and there are major developments to the monkey and otter enclosures. <br> </p>
+
+<h2> 2003 </h2>
+<p> Hocus Pocus Hall opens in the original Burnt Stub Mansion – a 4D walkthrough. <br> </p>
+
+<h2> 2004 </h2>
+<p> Land of the Dragons opens with an adventure play area for young toddles, as well as the spinsational Dragon’s Fury, a spinning coaster for all the family. <br> </p>
+
+<h2> 2005 </h2>
+<p> Children’s Zoo and Peeking Heights a slow moving observation wheel opens. Events are held all through the season, with special visits from the PowerPuff Girls, Dora the Explorer, Noddy and Scooby Doo. <br> </p>
+
+<h2> 2006 </h2>
+<p> Bubbleworks is revamped and sponsored by Imperial Leather. The ride is now the foam-fizzing, bubblebursting, sweet-smelling, gigglemaking, ducktastic Bubbleworks indoor family water ride. 2006 also sees the 75th anniversary of Chessington Zoo, and the opening of the Monkey & Bird Garden, a squirrel monkey walkthrough, and Creature Features. </p>
+
+<h2> 2007 </h2>
+<p> The merge of the Tussauds Group and Merlin Entertainments creates the Merlin Entertainments Group, the second largest visitor attraction operator in the world. <br>
+
+Holiday Inn Chessington opens, an exciting venture for the Park that attracts guests with contemporary design and a safari theme. <br> </p>
+
+<h2> 2008 </h2>
+<p> The new SEA LIFE centre is introduced to Chessington, housing over 20 displays with marine life ranging from starfish to stingrays. Chessington Zoo’s successful conservation work continues as it welcomes some landmarks for the UK: the first binturong baby born for over 10 years, and the first king vulture chick to be born. The Zoo also introduces two new Sumatran tiger cubs from parents Ratna and Batu– the pair’s second litter in just over two years, and a welcome addition to a species that has fewer than 400 left in the world. <br>
+
+Also introduced is a new silverback western lowland gorilla, Damisi, to Europe’s most successful breeding group. Transferred from Paignton Zoo, Damisi replaces long-standing alpha male Kumba. <br> </p>
+
+<h2> 2009 </h2>
+<p> Two new food outlets opened Market Square Pizza Pasta and the Fried Chicken Co. <br> </p>
+
+<h2> 2010 </h2>
+<p> Major investment in 2010 sees new land Wild Asia complete with the KOBRA ride open as Chessington’s showcase land. Wanyama Village & Reserve are created to expand the Zoo area of the Park and integrate the hotel as a resort. <br> </p>
+
+<h2> 2011 </h2>
+<p> The Resort’s incredible SEA LIFE centre welcomes some fearsome new neighbours: eight sharks, including four terrifying-looking blacktip reef sharks. <br>
+
+Chessington also updates the attraction with AZTECA, a mysterious underwater city for the sharks to explore. <br>
+
+The Zafari Bar & Grill opens at the Holiday Inn Chessington, offering guests culinary delights while they soak up the sunset and watch zebras, oryx and gazelles roaming the grounds of the picturesque Wanyama Village & Reserve. <br> </p>
+
+<h2> 2012 </h2>
+<p> DreamWorks Madagascar Live! Prepare to Party opens in the new land Africa. Flying Jumbos remain open but moved to Forbidden Kingdom. Two new food outlets opened Original Sandwich Co. and The Coffee Hut. <br> </p>
+
+<h2> 2013 </h2>
+<p> Chessington opens ZUFARI: Ride into Africa! a safari truck expedition into a mysterious African Kingdom. Amongst other animals ZUFARI brings Giraffe back to the park after an 18 year absence and introduces White Rhino for the first time. <br>
+
+Two new food outlets opened in Transylvania, Vampire’s Burger Kitchen and Creaky Café. <br> </p>
+
+<h2> 2014 </h2>
+<p> The Resort has seen the biggest investment ever with nearly £15 million spent across the estate. Launching a brand new ride experience for Scorpion Express, brand new AMAZU Treetop Adventure and the launch of the Azteca Hotel, a fully themed Resort hotel located on park with 69 amazing new themed rooms, a new splash water play zone within the leisure club and a totally unique temple-themed restaurant. <br> </p>
+
+<h2> 2015 </h2>
+<p> Chessington celebrated the Year of the Penguins, launching an exclusive live show ‘Penguins of Madagascar Live: Operation Cheezy Dibbles’ and creating two new fully themed Penguins of Madagascar bedrooms. <br>
+
+The Resort also welcomed a newly themed Penguins of Madagascar ride for young adventurers and a brand new enclosure for our resident Humboldt penguins, including a walk-through experience. <br> </p>
+
+<h2> 2016 </h2>
+<p> In a world first, Chessington launched Pandamonium LIVE show, featuring life-size and life-like animatronic pandas. Trail of the Kings was re-vamped and introduced Kamal, an endangered Asiatic male lion. In the same year, Kamal and Rani successfully bred 3 lion cubs, a major success for Chessington Zoo’s breeding programme. <br>
+
+Glamping and Go Ape opened at Chessington, along with a new restaurant, Smokehouse Bar and Grill. Chessington also launched a brand new mobile app. <br> </p>
+
+<h2> 2017 </h2>
+<p> The Gruffalo River Ride Adventure opened in Wild Woods (previously Transylvania), replacing family favourite Bubbleworks after 26 years. The Gruffalo was welcomed across the Resort with The Gruffalo Arena, The Gruffalo Gift Shop, The Gruffalo Bites and new fully themed bedrooms within Safari Hotel.The Adventure Tree also opened in Adventure Point (previously Market Square). <br> </p>
+
+<h2> 2018 </h2>
+<p> The multi-million pound Land of the Tiger, featuring an innovative enclosure and Europe’s first overhead trails, home to a family of four endangered Amur Tigers. <br>
+
+The project took eighteen months of meticulous planning and preparation to ensure Land of the Tiger was fit for the four felines. The Zoo’s Carnivore Keepers worked closely with experts to design this custom-built space for the big cats to thrive in, and comprises the largest investment in the Resort’s Zoo in five years. <br>
+
+Four new tiger themed rooms also opened in the Chessington Safari Hotel. <br> </p>
+
+<h2> 2019 </h2>
+<p> Room on the Broom - A Magical Journey launched as an enchanting addition to the Resort for spring 2019. The attraction brings to life yet another of author Julia Donaldson and illustrator Axel Scheffler’s  much-loved children’s stories, following in the footsteps of the Resort’s Gruffalo River Ride Adventure.Two new themed rooms also opened in the Chessington Safari Hotel. <br> </p>
+</div>
 
 <style>
     h1 { color: #007bff; }
